da73709aaf3fecf062a0ba28e3437c8b6c6e739e
Adrien SIEG <31514820+adriensieg@users.noreply.github.com>
2024-12-09 11:22:02 -0600

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['localhost'], // Add any external domains if needed
+    },
+};
+
+module.exports = nextConfig;
